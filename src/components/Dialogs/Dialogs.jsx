cfeb02a0bc1cb2fdb08d6642777c21fde0910cab
Vladislav_Stoyanov <laducha@inbox.ru>
2021-09-19 14:42:49 +0300

    modified:   src/App.js 	modified:   src/components/Dialogs/Dialogs.jsx 	modified:   src/components/Profile/MyPosts/MyPosts.jsx 	modified:   src/components/Profile/Profile.jsx 	modified:   src/index.js 	modified:   src/redux/state.js 	deleted:    src/render.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import s from "./Dialogs.module.css";
 import Message from "./Message/Message";
 import Dialog from "./Dialog/Dialog";
+import {onMessageChangeActionCreator, sendMessageActionCreator} from "../../redux/state";
+
+
+
 
 const Dialogs = (props) => {
+
   let dialogsItems = props.state.dialogs.map((d) => (
     <Dialog name={d.name} id={d.id} />
   ));
@@ -15,9 +20,14 @@ const Dialogs = (props) => {
   let newMessageElement = React.createRef();
 
   let sendMessage = () => {
-    let text = newMessageElement.current.value;
-    alert(text);
+    props.dispatch(sendMessageActionCreator());
   };
+
+  let onMessageChange = () => {
+    let text = newMessageElement.current.value;
+    props.dispatch(onMessageChangeActionCreator(text))
+  }
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs.items}>{dialogsItems}</div>
@@ -25,7 +35,7 @@ const Dialogs = (props) => {
       <div className={s.blockOfMessages}>
         <div className={s.messages}>{messagesItems}</div>
         <div className={s.newMessageAdd}>
-          <textarea ref={newMessageElement}></textarea>
+          <textarea onChange={onMessageChange} ref={newMessageElement} value={props.state.newMessage}></textarea>
           <button onClick={sendMessage}>Send</button>
         </div>
       </div>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,48 +1,108 @@
-import { rerenderEntireTree } from "../render";
-
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likes: 15 },
-      { id: 2, message: "My name is Vladislav.", likes: 20 },
-      { id: 3, message: "It's my first post!", likes: 20 },
-    ],
-    newPostText: 'way of Samurai',
+const ADD_POST = 'ADD-POST'
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
+const SEND_MESSAGE = 'SEND-MESSAGE'
+const ON_MESSAGE_CHANGE = 'ON-MESSAGE-CHANGE'
+let store = {
+  _state : {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likes: 15 },
+        { id: 2, message: "My name is Vladislav.", likes: 20 },
+        { id: 3, message: "It's my first post!", likes: 20 },
+      ],
+      newPostText: 'way of Samurai',
+    },
+    dialogsPage: {
+      dialogs: [
+        { name: "Dimych", id: "1" },
+        { name: "Sasha", id: "2" },
+        { name: "Andrey", id: "3" },
+        { name: "Victor", id: "4" },
+      ],
+      messages: [
+        { text: "Hi" },
+        { text: "Hello" },
+        { text: "How are you?" },
+        { text: "I'm fine ,and you?" },
+      ],
+      newMessage: 'Hello'
+    },
   },
-  dialogsPage: {
-    dialogs: [
-      { name: "Dimych", id: "1" },
-      { name: "Sasha", id: "2" },
-      { name: "Andrey", id: "3" },
-      { name: "Victor", id: "4" },
-    ],
-    messages: [
-      { text: "Hi" },
-      { text: "Hello" },
-      { text: "How are you?" },
-      { text: "I'm fine ,and you?" },
-    ],
+  _callSubscriber () {
+
   },
-};
+  getState(){
+  return this._state
+},
+  subscribe (observer) {
+    this._callSubscriber = observer
+  },
 
 
-export let addPost = () => {
-  debugger;
-  let newPost = {
-    id: 4,
-    message: state.profilePage.newPostText,
-    likes: 0,
-  };
+  dispatch(action) {
+    if(action.type === ADD_POST){
+      debugger
+      let newPost = {
+        id: 4,
+        message: this._state.profilePage.newPostText,
+        likes: 0,
+      }
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = ''
+      this._callSubscriber(this._state);
+    }else if(action.type === UPDATE_NEW_POST_TEXT){
+      debugger
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);// переотрисовка
+    }else if(action.type === SEND_MESSAGE){
+      let newMessage = {
+        text: this._state.dialogsPage.newMessage,
+      }
 
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = ''
-  rerenderEntireTree(state);
-};
+      this._state.dialogsPage.messages.push(newMessage);
+      this._state.dialogsPage.newMessage = ''
+      this._callSubscriber(this._state);
+    }else if(action.type === ON_MESSAGE_CHANGE){
+      this._state.dialogsPage.newMessage = action.newTextMessage;
+      this._callSubscriber(this._state);
+    }
 
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
 
-export default state;
+  }
+
+}
+
+
+
+
+export const addPostActionCreator= () =>{
+  return {
+    type: ADD_POST
+  }
+}
+export const updateNewPostTextActionCreator = (text) =>{
+  return{
+    type:UPDATE_NEW_POST_TEXT,newText:text
+  }
+}
+
+
+export const sendMessageActionCreator= () =>{
+  return{
+    type:SEND_MESSAGE
+  }
+}
+export const onMessageChangeActionCreator= (text) =>{
+  return{
+    type:ON_MESSAGE_CHANGE,
+    newTextMessage:text
+  }
+}
+
+
+
+
+
+
+
+export default store;
