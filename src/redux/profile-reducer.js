be60f6868d1190c4215b2f1c7720fdb9bd3d19ae
Vladislav_Stoyanov <laducha@inbox.ru>
2021-09-23 14:03:17 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Profile/Profile.jsx 	modified:   src/index.js 	modified:   src/redux/dialogs-reducer.js 	modified:   src/redux/profile-reducer.js 	new file:   src/redux/redux-store.js 	renamed:    src/redux/state.js -> src/redux/store.js

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,8 +1,16 @@
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 
+let initialState = {
+    posts: [
+        { id: 1, message: "Hi, how are you?", likes: 15 },
+        { id: 2, message: "My name is Vladislav.", likes: 20 },
+        { id: 3, message: "It's my first post!", likes: 20 },
+    ],
+    newPostText: 'way of Samurai',
+}
 
-const profileReducer = (state,action) => {
+const profileReducer = (state = initialState,action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
